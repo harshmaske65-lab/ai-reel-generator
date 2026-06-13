0d1a708968fae19133eb1b4a4ecac4e561601965
Harsh Maske <maskesacheen5@gmail.com>
2026-06-13 23:31:45 +0530

    Added create page and Flask routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,8 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/create")
+@app.route("/create",methods=["GET","Post"])
 def create():
+    if request.method == "POST":
+        print("FORM:", request.form)
+        print("FILES:", request.files)
+        print("FILE1:", request.files.get("file1"))
+    
     return render_template("create.html")
 
 @app.route("/gallery")
